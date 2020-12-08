00660a0dc6da6a4c85e3c8abafba512fe19be575
Jin Kuwata <jmk2142@tc.columbia.edu>
2020-12-08 00:27:55 -0500

    Added Project Names

--- scripts/teams.js
+++ scripts/teams.js
@@ -1,69 +1,71 @@
 let teams = [
   {
     teamName: "Sand Sculptors",
-    projectName: "Project AAA",
-    youtubeURL: "",
-    youtubeID: "",
+    projectName: "Sand Sculptors",
+    youtubeURL: "https://youtu.be/GUTRugWcxR0",
+    youtubeID: "GUTRugWcxR0",
     videoFileURL: "https://drive.google.com/drive/folders/16Y__XGXRO9rtqaDr4bCIXZPRON5SEb4q?usp=sharing",
     summary: "Learners often struggle self-regulate their learning processes in online environments. We aim to provide a tool to improve self-regulated learning for learners struggling with e-Learning environments.",
     members: ["Ningcong Tong","Yichen Yao","Jiaen Zhang","Kristin Wang","You Lyu"]
   },{
     teamName: "roARgh",
-    projectName: "Project BBB",
+    projectName: "StorySky",
     youtubeURL: "https://youtu.be/XElmuKWoW4I",
     youtubeID: "XElmuKWoW4I",
     videoFileURL: "",
-    summary: "COVID has forced us all to realize the importance of our relationships. We reconnect families over distance through the telling of personal stories, providing an interactive story-time experience to rebuild those bonds and create a sort of keepsake of those fleeting memories.",
+    summary: "COVID has ... forced us all to realize the importance of our relationships for our social, emotional, and cognitive development and well-being. Our goal is to use the technology we have to reconnect families over distance through the telling of personal stories, providing an interactive story-time experience to rebuild those bonds and create a sort of keepsake of those fleeting memories.",
     members: ["David Zikovitz","Ying-Chen Lin","Mulan Fu","Marina Lemee"]
   },{
     teamName: "Double X",
-    projectName: "Project CCC",
+    projectName: "Science of Life",
     youtubeURL: "https://youtu.be/UwibA-SRPZM",
     youtubeID: "UwibA-SRPZM",
     videoFileURL: "",
-    summary: "A key aspect of our product is that it connects real life experiences from the students with school science. Our survey directed at Brazilian teachers and students showed great interset in connecting formal knowledge to phenomena observed in everyday life.",
+    summary: "A key aspect of our product is that it connects real life experiences from the students with school science. Our survey directed at Brazilian teachers and students showed great interest in connecting formal knowledge to phenomena observed in everyday life.",
     members: ["HyunSoo Suh","Kelley Cameron","Fernanda Albuquerque de Araujo"]
   },{
     teamName: "Musitation",
-    projectName: "Project DDD",
+    projectName: "Musitation",
     youTubeURL: "https://youtu.be/8Cn80C9XAzI",
     youtubeID: "8Cn80C9XAzI",
     videoFileURL: "https://drive.google.com/file/d/1axBldv7aL86dS1_eYehfnZLk1vW0GRP0/view?usp=sharing",
-    summary: "The goal of Musitation is to support meaningful social interaction through music and to inspire people to creatively express themselves through collaborative VR music experiences.",
+    summary: "After COVID, people who have an interest in music creation are afraid of continuing in-person social musical activities although they desire immersive music learning and meaningful interpersonal interactions. The goal of Musitation is to support meaningful social interaction through music and to inspire people to creatively express themselves through collaborative VR music experiences.",
     members: ["Fei Wang","Daniel Ahn","Anh-Thu Phan","Calley Nelson","Jason Reid"]
   },{
-    teamName: "C PlaN",
-    projectName: "Project EEE",
+    teamName: "C Plan",
+    projectName: "LinguaBuddy",
     youTubeURL: "https://youtu.be/k8UE2y-FZsk",
     youtubeID: "k8UE2y-FZsk",
     videoFileURL: "",
-    summary: "The lack of student-to-student and student-to-teacher interactions during online language teaching is a problem. Our goal is to help language teachers prepare different class activities in a virtual learning environment.",
+    summary: "The lack of student-to-student and student-to-teacher interactions during online language teaching is a problem. Our goal is to help language teachers prepare different class activities in a virtual learning environment; create a virtual activity space to engage students; and to help students alleviate learning anxiety during activities.",
     members: ["Hengzhi Huang","Rong Sang","Danning Fu","Weiwei Ma","Yuxuan Huang"]
   },{
     teamName: "Grassroots",
-    projectName: "Project FFF",
+    projectName: "Grassroots",
     youTubeURL: "https://youtu.be/W1hMVQH2x28",
     youtubeID: "W1hMVQH2x28",
     videoFileURL: "",
-    summary: "Grassroots is an app that promotes sustained engagement and intergenerational cooperation in organization-based activism. Grassroots builds an intergenerational community and validates the advocacy efforts of both young and old.",
+    summary: "Activists' ageist perspectives toward other activities and activist activities keep organizations from maximizing their impact. Grassroots is an app that promotes sustained engagement and intergenerational cooperation in organization-based activism. Grassroots builds an intergenerational community and validates the advocacy efforts of both young and old.",
     members: ["Dennis Morgan","Echo Zhang","Uttarika Shetty","Nancye Blair Black","Hongjin Du"]
   },{
     teamName: "It's Okay",
-    projectName: "Project GGG",
+    projectName: "Project Name HERE",
     youTubeURL: "",
     youtubeID: "",
     videoFileURL: "https://drive.google.com/file/d/1aPVOk6j7gBLKxBjclPJI78dKo9HhZJWU/view?usp=sharing",
-    summary: "",
+    summary: "All intimate relationships involve some degrees of conflict. We've designed an interactive journal that helps users practice mindfulness, document and track their emotional patterns along with an AI-empowered chatbot system that provides relationship consultation.",
     members: ["Lingyi Zhuo","Zhuoda Wu","Bora Shin"]
   },{
     teamName: "WETech",
-    projectName: "Project HHH",
-    youTubeURL: "",
+    projectName: "WETech",
+    youTubeURL: "https://youtu.be/AJV8kUSjYKA",
+    youtubeID: "AJV8kUSjYKA",
     videoFileURL: "",
-    summary: "",
+    summary: "We situate learners in VR scenarios designed using Task-Based Language Teaching (TBLT) with a focus on pragmatics, so that learners practice with relevant tasks that they need to accomplish in real life in socially appropriate ways.",
     members: ["Jiajin Han","Rundong Jiang","Samantha Sowah","Alex Wang","Crystal Yang"]
   }
 ];
+
 
 for(let i = teams.length - 1; i > 0; i--){
   const j = Math.floor(Math.random() * i)
@@ -128,4 +130,19 @@ function makeTeamHTML(data, alt=false) {
     </div>`
   }
   return HTML;
+}
+
+// START_TIME = Tue Dec 08 2020 18:00:00 GMT-0500 (Eastern Standard Time)
+
+const START_TIME = 1607468400000;
+let timeNow = Date.now();
+if (timeNow > START_TIME) {
+  enableLinks();
+}
+
+function enableLinks() {
+  let sectionEls = [...document.querySelectorAll(".vote-section-hidden")];
+  sectionEls.forEach(el => {
+    el.classList.remove('vote-section-hidden');
+  });
 }
