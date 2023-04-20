15d4c16a76164f7da70f8c625bd0a5d3bdca1135
Ethan Bookstein <Alostrabbit@gmail.com>
2023-04-20 01:19:58 -0400

    Main event in navbar, updated event page + index

---
+++ scripts/teams2023.js
@@ -0,0 +1,152 @@
+// let winners = [
+//     {
+//       teamName: "Melionaire",
+//       projectName: "Dollar Dollar",
+//       documentURL: "Melionaire.pdf",
+//       youtubeURL: "https://youtu.be/pjuJPCd0pZk",
+//       youtubeID: "pjuJPCd0pZk",
+//       videoFileURL: "",
+//       summary: "In the Metaverse, where the virtual and the real world are converging, and hence the economies and finances of both worlds, our understanding of this financial convergence is still lacking. Through our design, we hope to bridge that gap through creating a safe environment in the Metaverse where learners get to practice and develop their financial literacy skills.",
+//       members: ["Keying Chen","Anqi Wang","Abu Abdelbagi","Xinyan (Helen) Song"],
+//       award: "INA 2023 Award"
+//     },{
+//       teamName: "Cerberus",
+//       projectName: "Soteria",
+//       documentURL: "Cerberus.pdf",
+//       youTubeURL: "https://youtu.be/WMKj9VgsUiA",
+//       youtubeID: "WMKj9VgsUiA",
+//       videoFileURL: "",
+//       summary: "Our design supports social connectedness in order to improve employee well-being. Both regulation of emotions and genuine self-disclosure have been shown to enhance one's well-being (Luo & Hancock, 2020; Restubog et al., 2020). In having a place to share and participate in shared pain points of remote work isolation and mental wellness, remote workers can partake in a larger community and have access to advice on mental wellness in the remote workspace.",
+//       members: ["Fei Wang","Daniel Ahn","Anh-Thu Phan","Calley Nelson","Jason Reid"],
+//       award: "People's Choice Award"
+//     }
+//   ];
+  let teams = [
+    {
+      teamName: "News AI",
+      projectName: "Veracity Social",
+      documentURL: "",
+      youtubeURL: "https://youtu.be/IjU9JTaCFUY",
+      youtubeID: "IjU9JTaCFUY",
+      videoFileURL: "",
+      summary: "The influx of information and narratives makes it difficult for users to verify the validity of videos they see on social media. This tool strips the bias from content so that users can more objectively understand the facts, provides multiple perspectives to break epistemic bubbles and echo chambers, and facilitates critical thinking with critical questions.",
+      members: ["Stella Guan", "Rhea Jaffer", "Megan Kim"]
+    },{
+      teamName: "Co-Chat Visionaries",
+      projectName: "Co-Chat",
+      documentURL: "",
+      youtubeURL: "https://youtu.be/O8_zQcq_EqA",
+      youtubeID: "O8_zQcq_EqA",
+      videoFileURL: "",
+      summary: "Co-chat is an app which is dedicated to creating an intuitive coffee chat space from a multi-role perspective and to fulfilling user needs. As coffee chats have become increasingly popular in recent years, some graduates still need clarification about accessing adequate and appropriate coffee chats for their careers. Our app purports to bridge an informative dialogue between graduates and professionals within startups to create a convenient space.",
+      members: ["Jiaqi (Sabrina) Li", "Siqi Chen", "Yuhan Li", "Yifei Jiang"]
+    },{
+      teamName: "EdYouTechnovate",
+      projectName: "TeachGenie",
+      documentURL: "",
+      youtubeURL: "https://youtu.be/kRf0VHtmRXg",
+      youtubeID: "kRf0VHtmRXg",
+      videoFileURL: "",
+      summary: "Teachers are responsible for several responsibilities including planning lessons, grading assignments, and managing administrative tasks, which can lead to burnout and high levels of stress. With aims to reduce the workload for teachers, we designed a mobile web app, TeachGenie, that serves as an asynchronous professional development opportunity to introduce to teachers the affordances of AI language models and possibilities of integration into their teaching practices. TeachGenie utilizes two essential approaches to professional development, experiential learning and learning communities, by providing tasks and challenges for teachers to experiment with AI language models, as well as an interaction feature for individual teachers to connect and discuss with other users.",
+      members: ["Ho Sue Kwon", "Zainab Mesiya", "Vickie Liou"]
+    },{
+      teamName: "IN AI pickle",
+      projectName: "IN AI pickle",
+      documentURL: "",
+      youtubeURL: "https://youtu.be/4SamUm5VymQ",
+      youtubeID: "4SamUm5VymQ",
+      videoFileURL: "",
+      summary: "AI art stirs ethical debate over its impact on digital artists and copyright law. Instead of advocating for a specific stance, we created a narrative game where players assume the role of an AI bot and consider different perspectives on human vs. AI generated art. They are presented <q>Trolley-Problem-lik</q> scenarios which compel them to think about how they value art: is it about a pretty picture? an affordable one? or is it fundamentally about the humanity in creative processes?",
+      members: ["Titus Chen", "Ethan Bookstein", "Wentao Wu", "Sharleen Loh", "Nancye Blair Black"]
+    },{
+      teamName: "DEI DIY",
+      projectName: "DEI DIY",
+      documentURL: "",
+      youtubeURL: "https://youtu.be/gS3CLUXdvEg",
+      youtubeID: "gS3CLUXdvEg",
+      videoFileURL: "",
+      summary: "Everyone has unconscious biases and makes snap judgments about others. DEI DIY is a three-part training program with activities and discussions for experienced teachers to realize their unconscious biases and have a safe space to practice holding back snap judgments. In Part 1, teachers choose between two students to select into a science competition team; in Part 2, teachers create avatars based on descriptions of students; and in Part 3, teachers are tasked with teaching a virtual classroom of avatar students to practice holding back unconscious biases.",
+      members: ["Haeram Jang", "Ray Chen", "Kazuki Saito", "Suan Kang"]
+    },{
+      teamName: "Sparkling",
+      projectName: "Art Spinner: A whole new museum experience",
+      documentURL: "",
+      youtubeURL: "https://youtu.be/BLePph3bj4M",
+      youtubeID: "BLePph3bj4M",
+      videoFileURL: "",
+      summary: "Art Spinner brings a whole new museum experience to users by providing services before, during, and after their museum journey. It generates customized visiting experience and creates private gallery to maintain the personal sense of art. But also builds a community for art lovers and museum explorers to share their perspectives and collide with inspirations.",
+      members: ["Yuening Yu", "Jingya Zhu", "Ziyu Wei"]
+    },{
+      teamName: "Pocket Museum: Introducing Field Trips for the Classroom",
+      projectName: "Curiostory",
+      documentURL: "",
+      youtubeURL: "https://youtu.be/lbkrMB0beHw",
+      youtubeID: "lbkrMB0beHw",
+      videoFileURL: "",
+      summary: "The Pocket Museum provides a novel way for students to engage with museum learning virtually. With this platform, teachers can work alongside museum educators to customize virtual field trips, whether by creating new ones or modifying existing trips from other teachers or museum educators. As a result, students can enjoy an immersive and exciting educational experience.",
+      members: ["Andrea Maidanik", "Sijia Rebecca Cheng", "Jiayi Xue", "Sadia Ahmed", "Yi Chen"]
+    },{
+        teamName: "Path Weaver",
+        projectName: "Rootmap - Learn, Share, Connect",
+        documentURL: "",
+        youtubeURL: "https://youtu.be/xoc9qYG8Y7I",
+        youtubeID: "xoc9qYG8Y7I",
+        videoFileURL: "",
+        summary: "RootMap is a social app designed to create a safe and welcoming space for individuals and families to learn, share, and connect through personal and family history across time and place. The app's interactive globe feature allows users to upload personal stories and visualize their unique geographical path, creating an engaging way for others to explore their content. With advanced search functions and easy-to-use connection features, users can find people and content based on geohistorical information and connect with them effortlessly.",
+        members: ["Lexuan Li", "Wanyu Wu", "Hualin Wu", "Qian Xu", "Xiaoqi Zhang"]
+      },{
+        teamName: "PsyComm",
+        projectName: "Good Mourning",
+        documentURL: "",
+        youtubeURL: "https://youtu.be/fz5qpkd-3oA",
+        youtubeID: "fz5qpkd-3oA",
+        videoFileURL: "",
+        summary: "Good Mourning is a digital interactive experience designed to informally educate people about the diverse ways of processing grief and to support them in their unique grief experience. The user is a caretaker of a small cemetery where they are able to create a personalized memorial to express their grief and remembrance of their passed loved on. They are also able to read and engage with the stories of other people who have gone through grief, learning that grief can be expressed and manifest in many different emotions and behaviors and that they are not alone.",
+        members: ["Molly Friedel", "Shuhan Zhang", "Victoria Wu", "Yiran Wang"]
+      }
+  ];
+  
+  // RAMDOMIZE THE ARRAY
+  for(let i = teams.length - 1; i > 0; i--){
+    const j = Math.floor(Math.random() * i)
+    const temp = teams[i]
+    teams[i] = teams[j]
+    teams[j] = temp
+  }
+  
+  // Function that CONSTRUCTS HTML TEMPLATE STRING
+  // Interpolate team data into the template
+  function makeTeamHTML(data) {
+    let HTML = `<div class="row" style="margin-bottom:5em;">
+      <div class="col-sm-6 col-xs-12" style="background-color:#1b3d6e;padding:1em;">
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe src="https://www.youtube.com/embed/${data.youtubeID}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      </div>
+      <div class="col-sm-6 col-xs-12" style="padding:1em;">
+        <h2 style="margin-top:0;font-family:'Roboto', sans-serif;font-size:2.25em;font-weight:500;">${data.projectName}</h2>
+        <h3 style="margin-top:.75em;">Team: ${data.teamName}</h3>
+        <p style="margin-top:1em;"><em>${data.members.join(", ")}</em></p>
+        <p>${data.summary}</p>
+      </div>
+    </div>`;
+  
+    // <p style="margin-top:1.5em;">
+    // <a style="color:white;" href="./assets/INA2022/Documents/${data.documentURL}" download><i class="fa fa-fw fa-file-pdf-o"></i>&nbsp;Download &mdash; ${data.projectName} Research and Design Summary</a>
+    // </p>
+  
+    return HTML;
+  }
+  
+  
+  // CONSTRUCT THE HTML
+  let teamsHTML = "";
+  
+  teams.forEach(function(team) {
+    if (team.youtubeID) {
+      teamsHTML += makeTeamHTML(team);
+    }
+  });
+  
+  // ADD it to the View State (page)
+  document.querySelector("#project-rows").innerHTML = teamsHTML;
